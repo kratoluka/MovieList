240463ba9736517167c37984e934d507a4e4bd86
kratoluka <kratoluka@gmail.com>
2022-10-07 15:35:01 +0200

    Add database model for movie

--- app/MovieListApp/models.py
+++ app/MovieListApp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Movie(models.Model):
+    name = models.CharField(max_length=200)
+    short_name = models.CharField(max_length=100)
+    description = models.CharField(max_length=1000)
+    icon_url = models.CharField(max_length=200)
+    manifest_url = models.CharField(max_length=200)
+    disabled = models.BooleanField()
+
+    def __str__(self):
+        return self.name
