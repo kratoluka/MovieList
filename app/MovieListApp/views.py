464e22820b5ccbcc65553dbf239c34fb6a9bfe07
kratoluka <kratoluka@gmail.com>
2022-10-08 10:25:39 +0200

    Add view to display movies

--- app/MovieListApp/views.py
+++ app/MovieListApp/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Movie
 
-# Create your views here.
+
+def index(request):
+    movie_list = Movie.objects.all()
+    return render(request, 'index.html', {'movie_list': movie_list})
