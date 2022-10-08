145b2462887d17f764ae313f1f7a2bec503b114c
kratoluka <kratoluka@gmail.com>
2022-10-08 12:14:59 +0200

    Add sort and filter options

--- app/MovieListApp/views.py
+++ app/MovieListApp/views.py
@@ -1,7 +1,50 @@
 from django.shortcuts import render
 from .models import Movie
+from datetime import datetime, timedelta
 
 
 def index(request):
+    sort_options = {
+        'date_added': 'Date added',
+        'name': 'Name',
+        'is_featured': 'Featured'
+    }
+    filter_options = {
+        '': 'None',
+        'is_featured': 'Featured',
+        'new_this_week': 'This week',
+        'new_this_month': 'This month',
+        'new_this_year': 'This year'
+    }
+
+    sort = request.GET.get('sort', 'date_added')
+    filt = request.GET.get('filter', '')
+
     movie_list = Movie.objects.all()
-    return render(request, 'index.html', {'movie_list': movie_list})
+
+    if sort:
+        if sort == 'is_featured':
+            movie_list = movie_list.order_by('-is_featured')
+        else:
+            movie_list = movie_list.order_by(sort)
+
+    if filt:
+        now = datetime.now()
+        if filt == 'is_featured':
+            movie_list = movie_list.filter(is_featured=True)
+        elif filt == 'new_this_week':
+            movie_list = movie_list.filter(date_added__range=[now - timedelta(days=7), now])
+        elif filt == 'new_this_month':
+            movie_list = movie_list.filter(date_added__range=[now - timedelta(days=30), now])
+        elif filt == 'new_this_year':
+            movie_list = movie_list.filter(date_added__range=[now - timedelta(days=365), now])
+
+    context = {
+        'movie_list': movie_list,
+        'sort': sort,
+        'filter': filt,
+        'sort_options': sort_options,
+        'filter_options': filter_options
+    }
+
+    return render(request, 'index.html', context)
